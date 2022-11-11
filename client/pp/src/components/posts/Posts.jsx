534b51a0d9197316b907ee682c20550e346037b8
innazozulia <innazozulia@outlook.com>
2022-11-11 20:31:09 +0200

    added posts

--- client/pp/src/components/posts/Posts.jsx
+++ client/pp/src/components/posts/Posts.jsx
@@ -11,29 +11,34 @@ const Posts = () => {
       return res.data;
     });
   });
+  // const { isLoading, error, data } = useQuery(["posts"], () => {
+  //   makePequest.get("/posts").then((res) => {
+  //     return res.data;
+  //   });
+  // });
 
-  //TEMPORARY
+  TEMPORARY;
 
-  // const posts = [
-  //   {
-  //     id: 1,
-  //     name: "Emma Murphy",
-  //     userId: 1,
-  //     profilePicture:
-  //       "https://images.unsplash.com/photo-1665686304355-0b09b1e3b03c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-  //     img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
-  //   },
+  const posts = [
+    {
+      id: 1,
+      name: "Emma Murphy",
+      userId: 1,
+      profilePicture:
+        "https://images.unsplash.com/photo-1665686304355-0b09b1e3b03c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
 
-  //   {
-  //     id: 2,
-  //     name: "Emma Murphy",
-  //     userId: 2,
-  //     profilePicture:
-  //       "https://images.unsplash.com/photo-1665686304355-0b09b1e3b03c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-  //     desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
-  //   },
-  // ];
+    {
+      id: 2,
+      name: "Emma Murphy",
+      userId: 2,
+      profilePicture:
+        "https://images.unsplash.com/photo-1665686304355-0b09b1e3b03c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+    },
+  ];
 
   return (
     <div className="posts">
